9cffa8afa6c9a042c5561dd042c64532010cff66
Aditya Balwani <aditya@buncee.com>
2016-09-13 15:08:48 -0400

    Website updates

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
+
 var main = function() {
-   $(".button-collapse").sideNav();
+    $('.parallax').parallax();
+    $(".button-collapse").sideNav();
 };
+
+window.onscroll = function() {
+    var scrollTop = $(window).scrollTop();
+    console.log(scrollTop);
+    var opacity = Math.min(scrollTop/440, 1);
+    console.log(opacity);
+    $(".section-foreground-overlay").css('background-color', 'rgba(30,30,30,'+opacity+')')
+    if(scrollTop > 440) {
+        $("nav").css('background-color', 'rgba(30,30,30,1)')
+    } else {
+        $("nav").css('background-color', 'rgba(30,30,30,0)')
+    }
+}
 
 $(document).ready(main);
